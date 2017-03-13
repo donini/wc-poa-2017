6abf31482a919f482be16c1f6e145667e7033df3
Eduardo Pittol <edpittol@gmail.com>
2017-03-13 01:05:05 -0300

    First style edition

---
+++ theme/src/footer.php
@@ -0,0 +1,129 @@
+			<footer id="colophon" class="site-footer" role="contentinfo">
+				<div class="wrap">
+
+					<aside class="widget-area" role="complementary">
+						<div class="widget-column footer-widget-1">
+							<section id="wcpt_organizers-3" class="widget wcpt_widget_organizers">
+								<h2 class="widget-title">Organizadores</h2>
+
+								<div class="wcorg-organizers">
+									<div class="wcorg-organizer">
+										<h2>Eduardo Pittol</h2>
+										<div class="wcorg-organizer-description">
+											<img alt="" src="https://secure.gravatar.com/avatar/358a97cc31847c2c10f044e6c9c0e0d5?s=100&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/358a97cc31847c2c10f044e6c9c0e0d5?s=200&amp;d=mm&amp;r=g 2x" class="avatar avatar-100 photo grav-hashed grav-hijack" id="grav-358a97cc31847c2c10f044e6c9c0e0d5-0" width="100" height="100">
+										</div>
+									</div>
+
+									<div class="wcorg-organizer">
+										<h2>Gustavo Tagliassuchi</h2>
+										<div class="wcorg-organizer-description">
+											<img alt="" src="https://secure.gravatar.com/avatar/22923630b2517c45b9815fd9529a0564?s=100&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/22923630b2517c45b9815fd9529a0564?s=200&amp;d=mm&amp;r=g 2x" class="avatar avatar-100 photo grav-hashed grav-hijack" id="grav-22923630b2517c45b9815fd9529a0564-0" width="100" height="100">
+										</div>
+									</div>
+
+
+									<div class="wcorg-organizer">
+										<h2>Marco Andrei Kichalowsky</h2>
+										<div class="wcorg-organizer-description">
+											<img alt="" src="https://secure.gravatar.com/avatar/f6d11bb353784a4bd37ee12e37f1a16f?s=100&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/f6d11bb353784a4bd37ee12e37f1a16f?s=200&amp;d=mm&amp;r=g 2x" class="avatar avatar-100 photo grav-hashed grav-hijack" id="grav-f6d11bb353784a4bd37ee12e37f1a16f-0" width="100" height="100">
+										</div>
+									</div>
+
+
+									<div class="wcorg-organizer">
+										<h2>Rodrigo Donini</h2>
+										<div class="wcorg-organizer-description">
+											<img alt="" src="https://secure.gravatar.com/avatar/4825eb08511de0aacc49c965f9e11d41?s=100&amp;d=mm&amp;r=g" srcset="https://secure.gravatar.com/avatar/4825eb08511de0aacc49c965f9e11d41?s=200&amp;d=mm&amp;r=g 2x" class="avatar avatar-100 photo grav-hashed grav-hijack" id="grav-4825eb08511de0aacc49c965f9e11d41-0" width="100" height="100">
+										</div>
+									</div>
+								</div><!-- .wcorg-organizers -->
+
+								<a class="wcpt-organizers-link" href="https://2017.portoalegre.wordcamp.org/organizadores/">Equipe de organização →</a>
+							</section>
+						</div>
+						<div class="widget-column footer-widget-2">
+							<section id="wcb_sponsors-2" class="widget wcb_widget_sponsors"><h2 class="widget-title">Patrocinadores</h2>
+								<style>
+									.wcb_widget_sponsors .sponsor-logo {
+										display: block;
+										padding-bottom: 10px;
+									}
+
+									.wcb_widget_sponsors .sponsor-logo img {
+										max-width: 100%;
+										height: auto;
+
+										/* Conform to https://adblockplus.org/acceptable-ads */
+										max-height: 150px;
+										width: auto;
+									}
+								</style>
+
+
+								<div class="sponsor-level gold">
+									<h4 class="sponsor-level-title">Patrão</h4>
+
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/boldgrid/">
+										BoldGrid
+									</a>
+
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/woocommerce/">
+										WooCommerce
+									</a>
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/jetpack/">
+										Jetpack
+									</a>
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/bluehost/">
+										Bluehost
+									</a>
+								</div><!-- .sponsor-level -->
+
+								<div class="sponsor-level silver">
+									<h4 class="sponsor-level-title">Macanudo</h4>
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/godaddy/">
+										GoDaddy	
+									</a>
+
+								</div><!-- .sponsor-level -->
+
+								<div class="sponsor-level bronze">
+									<h4 class="sponsor-level-title">Guapo</h4>
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/sitelock/">
+										SiteLock
+									</a>
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/wpml/">
+										WPML
+									</a>
+
+									<a class="sponsor-logo" href="https://2017.portoalegre.wordcamp.org/sponsor/dreamhost/">
+										DreamHost
+									</a>
+
+								</div><!-- .sponsor-level -->
+
+							</section>
+						</div>
+					</aside><!-- .widget-area -->
+
+					<nav class="social-navigation" role="navigation" aria-label="Menu de links sociais do rodapé">
+						<div class="menu-redes-sociais-container"><ul id="menu-redes-sociais" class="social-links-menu"><li id="menu-item-95" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-95"><a href="https://www.facebook.com/WordcampPortoAlegre/"><span class="screen-reader-text">Facebook</span><svg class="icon icon-facebook" aria-hidden="true" role="img"> <use href="#icon-facebook" xlink:href="#icon-facebook"></use> </svg></a></li>
+						<li id="menu-item-94" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-94"><a href="https://twitter.com/WordCampPOA"><span class="screen-reader-text">Twitter</span><svg class="icon icon-twitter" aria-hidden="true" role="img"> <use href="#icon-twitter" xlink:href="#icon-twitter"></use> </svg></a></li></ul></div>
+					</nav><!-- .social-navigation -->
+					<div class="site-info">
+						<a href="https://wordpress.org/">Orgulhosamente desenvolvido com WordPress</a>
+					</div><!-- .site-info -->
+				</div><!-- .wrap -->
+			</footer>
+		</div><!-- .site-content-contain -->
+	</div><!-- #page -->
+	<?php wp_footer(); ?>
+
+</body>
+</html>
